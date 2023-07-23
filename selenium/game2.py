3e31ea30555079169c925f66bc83a205f5d395d6
Nathan <airialtw@gmail.com>
2023-07-23 13:16:01 +0800

    六合彩特3完成

--- selenium/game2.py
+++ selenium/game2.py
@@ -1,17 +1,7 @@
 import random
 from colorama import Fore, Style
 
-def lottery_numbers():
-    count = 0
-    results = []
-    while count < 1000:
-        first_six = random.sample(range(1, 50), 6)
-        seventh = random.choice([num for num in range(1, 50) if num not in first_six])
-        result = first_six + [seventh]
-        results.append(result)
-        count += 1
-    return results
-def judge(bets):
+def judge1(bets):
     if len(bets) != 7:
         return -1
     parsed_bets = [int(item) for item in bets]
@@ -22,12 +12,94 @@ def judge(bets):
     if special == 1:
         return 1
     return -1
-combo = lottery_numbers()
-#print(combo)    #列出300組
-for numbers in combo:
-    result = judge(numbers)
-    if result == 1:
-        print(Fore.RED + f"{numbers} 結果: {result}" + Style.RESET_ALL)
-    else:
-        print(f"{numbers} 結果: {result}")
+def lot35606():
+    count = 0
+    results = []
+    while count < 500:
+        six = random.sample(range(1, 50), 6)
+        seventh = random.choice([num for num in range(1, 50) if num not in six])
+        result = six + [seventh]
+        results.append(result)
+        count += 1
+        # 判斷並輸出結果
+        if judge1(result) == 1:
+            print(Fore.RED + f"{result} 結果: 1" + Style.RESET_ALL)
+        else:
+            print(f"{result} 結果: -1")
+    print("生成組數:", count)
+    return results
 
+def judge1(bets):
+    if len(bets) != 7:
+        return -1
+    parsed_bets = [int(item) for item in bets]
+    for bet in parsed_bets:
+        if not (1 <= bet <= 49):
+            return -1
+    special = parsed_bets[6]
+    if special == 2:
+        return 1
+    return -1
+def lot35607():
+    count = 0
+    results = []
+    while count < 500:
+        six = random.sample(range(1, 50), 6)
+        seventh = random.choice([num for num in range(1, 50) if num not in six])
+        result = six + [seventh]
+        results.append(result)
+        count += 1
+        # 判斷並輸出結果
+        if judge1(result) == 1:
+            print(Fore.RED + f"{result} 結果: 1" + Style.RESET_ALL)
+        else:
+            print(f"{result} 結果: -1")
+    print("生成組數:", count)
+    return results
+
+def judge1(bets):
+    if len(bets) != 7:
+        return -1
+    parsed_bets = [int(item) for item in bets]
+    for bet in parsed_bets:
+        if not (1 <= bet <= 49):
+            return -1
+    special = parsed_bets[6]
+    if special == 3:
+        return 1
+    return -1
+def lot35608():
+    count = 0
+    results = []
+    while count < 500:
+        six = random.sample(range(1, 50), 6)
+        seventh = random.choice([num for num in range(1, 50) if num not in six])
+        result = six + [seventh]
+        results.append(result)
+        count += 1
+        # 判斷並輸出結果
+        if judge1(result) == 1:
+            print(Fore.RED + f"{result} 結果: 1" + Style.RESET_ALL)
+        else:
+            print(f"{result} 結果: -1")
+    print("生成組數:", count)
+    return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    #lot35606()     #六合彩特碼1
+    #lot35607()     #六合彩特碼2
+    lot35608()
